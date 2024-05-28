288b1a8ba012e418b07b2f5d48a4ba86e6d9d461
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-05-28 23:22:10 +0900

    rock-scissors-paper in python

--- manage.py
+++ manage.py
@@ -2,18 +2,42 @@
 import random
 
 def get_choices():
+    # variable name = value (string)
   player_choice = input("Enter a choice (rock, paper scissors :")
-  # variable name = value (string)
+  #List store multiple items in single variable
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
   # Dictionary {key : value}
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
 
-choice = get_choices()
-print(choice)
+#parsing the data in function
+def check_win(player, computer):
+    #Concatenating
+    # print("You chose" + player + ", Computer chose" + computer)
+    print(f"You chose {player}, Computer chose {computer}")
+    if player == computer:
+        return "It's a tie!"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Player Win"
+        else:
+            return "You Lose"
+    elif player == "scissor":
+        if computer == "paper":
+             return "Player Win"
+        else:
+            return "You Lose"
+    elif player == "paper":
+        if computer == "rock":
+             return "Player Win"
+        else:
+            return "You Lose"
 
+choices = get_choices()
+result = check_win(choices["player"],choices["computer"])
+print(result)
 
-#List store multiple items in single variable
-# food =["pizza", "carrots","eggs"]
-# dinner = random.choice(food)
+# To get to the dic_data
+# choices = {"player" : "rock" , "computer" : "paper"}
+# p_choice = choices["player"]
