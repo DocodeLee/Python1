3f5a89e3cef49074d36d32565f714670a3a6771f
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-06-06 17:25:24 +0900

    input

---
+++ pythoncrashcourse/input.py
@@ -0,0 +1,52 @@
+## Input
+# message = input("Tell me :")
+# print(message)
+
+### clear prompts
+# name = input("Your name:")
+# print(f"\t Hello,{name}")
+
+## lines of prompt
+# a = "If there is something more want to say"
+# a += "\n Just use += and use the variable as prompt"
+
+# mess = input(a)
+# print(mess)
+
+# age = input("num:") # in the input computer get the Numercial as string
+# age >= 19
+
+## USE the int()
+# age = input("type your age :")
+# age = int(age)
+# if age >= 20:
+#     print("You are adult")
+# else:
+#     print("\n You will be able to ride after 20")
+
+## Modulo Operator
+# num = input("Enter num and i will tell if it's even or add:")
+# num = int(num)
+# if num % 2 == 0:
+#     print(f"{num} is even")
+# else:
+#     print(f"\n {num} is odd")
+
+
+## Practice
+# car = input("Which car do you want to rent:")
+# print(car)
+
+# num = input("How many peoples are in there:")
+# num = int(num)
+# if num > 8 :
+#     print("You need to wait")
+# else:
+#     print("The table is ready")
+
+# num = input("Checking the multiple of ten:")
+# num = int(num)
+# if num % 10 == 0:
+#     print("It is multiple of ten")
+# else:
+#     print("it is non related num")
