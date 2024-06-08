64f096b65f406501f6e16760f7d40335637a7239
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-06-08 20:34:39 +0900

    try,error

--- pythoncrashcourse/exception.py
+++ pythoncrashcourse/exception.py
@@ -96,22 +96,132 @@
 
 ##--------------------------------------
 ## 10 -4
-polling = True
-file = 'guest_book.txt'
-prompt = "Enter the user name"
-prompt += "('q' to quit) :"
-while polling:
+# polling = True
+# file = 'guest_book.txt'
+# prompt = "Enter the user name"
+# prompt += "('q' to quit) :"
+# while polling:
     
     
 
     
-    with open(file,"a") as file_obj:
-        user = input(prompt)
-        file_obj.writelines(f'\n{user}')
-        if user == "q":
-            break
+#     with open(file,"a") as file_obj:
+#         user = input(prompt)
+#         file_obj.writelines(f'\n{user}')
+#         if user == "q":
+#             break
     
-    with open(file,"r") as file_obj:
-        read = file_obj.readlines()
-        print(read)
+#     with open(file,"r") as file_obj:
+#         read = file_obj.readlines()
+#         print(read)
         
+
+
+## Exceptions
+
+## Python uses special objects called exceptions to manage errors
+## Exceptions are handled with try-except blocks
+
+# print(5/0) # In here youc can see the error "ZeroDivisionError"
+
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You cannot divide by zero")
+
+## simple calculator without handling error
+
+# print("give me two numm, I will divide them")
+# print("Enter 'q' to quit")
+
+# while True:
+#     first_num = input("\nFirst number: ")
+#     if first_num == 'q':
+#         break
+#     sec_num = input("\nSecond number: ")
+#     if sec_num == 'q':
+#         break
+#     try:
+#         answer = int(first_num) / int(sec_num)
+#     except ZeroDivisionError:
+#         print("You cannot divide by 0")
+#     except ValueError:
+#         print("Please enter the number")
+#     else:
+#         print(answer)
+
+## FileNotFoundError
+# filename = 'alice.txt'
+# try:
+#     with open(filename, encoding='utf-8') as f:
+#         content =f.read()
+# except FileNotFoundError:
+#     print(f"Sorry {filename} doesn't exist")
+
+# def count_words(filename):
+#     """Count the approximate number of words in file"""
+#     try:
+#         with open(filename, encoding='utf-8') as f:
+#             contents = f.read()
+#     except FileNotFoundError:
+#         # print(f"Sorry {filename} does not exist")
+#         pass
+#     else:
+#         words = contents.split()
+#         num_words = len(words)
+#         print(f"The file{filename} has about {num_words} words.")
+# filename=["guest_book.txt","ex.txt","guest.txt","alice.txt"]
+# for file in filename:
+#     count_words(file)
+
+
+## Practice
+##10-6 10-7
+
+# print("If you give two number i will add them")
+# print("if you want to stop please enter 'q'")
+
+# while True:
+#     first_num = input("Please enter First Number : ")
+#     if first_num == "q":
+#         break
+#     second_num = input("Please enter Second Number : ")
+#     if second_num == "q":
+#         break
+#     try:
+#         result = int(first_num) + int(second_num)      
+#     except ValueError:
+#         print("Please enter the Number")
+#     else:
+#         print(result)
+
+## 10-8
+
+# def readfile(filename):
+#     try:
+#         with open(filename,encoding="utf-8") as f:
+#             content = f.read()
+#             print(content)
+#     except FileNotFoundError:
+#          #print( "There is no File")
+#          pass
+
+# filenames = ['cats.txt','dogs.txt',"bird.txt"]
+# for filename in filenames:
+#     print(f"{filename}")
+#     readfile(filename)
+
+
+## ----------------------------------------
+## 10-9
+
+# file = "10_9.txt"
+# with open(file,encoding="utf-8")as f:
+#     content = f.read()
+#     the = content.lower().count("the ")
+#     print(the)
+
+
+##Storing Data
+## JSON data format is not specific to Python, so you can share data you
+## store in the JSON format with people who work in many other programming languages
